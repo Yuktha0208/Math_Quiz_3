b3accf8976e55df0aac4b7c89cf7f01ea8c17ed5
Yuktha0208 <71544862+Yuktha0208@users.noreply.github.com>
2020-12-01 18:13:15 +0530

    Add files via upload

---
+++ math_page.js
@@ -0,0 +1,9 @@
+function send(){
+    number1=document.getElementById("number1").value;
+    number2=document.getElementById("number2").value;
+    actual_ans=parseInt(number1)*parseInt(number2);
+
+    question_number="<h4>" + number1 + "X" + number2 +"</h4>";
+    input_box="<br> Answer: <input type='text' id='input_check_box'>"
+    check_button="<br> <br> <button class='btn btn-info' onclick='check()'>Check </button>"
+}
